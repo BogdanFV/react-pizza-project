e0e74fb4d0123304ef73adfe0902c277f96ba1e7
Bogdan <fbogdan13@gmail.com>
2022-08-29 15:18:24 +0300

    adaptive scss

--- src/App.js
+++ src/App.js
@@ -3,42 +3,26 @@ import React from "react";
 import './scss/app.scss';
 
 import Header from './components/Header'
-import Categories from './components/Categories'
-import Sort from './components/Sort'
-import PizzaBlock from "./components/PizzaBlock";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart"
+import NotFound from "./pages/NotFound";
+
+import './scss/app.scss'
+import { Route, Routes } from "react-router-dom";
+
 
 
 
 function App() {
-
-  const [items, setItems] = React.useState([]);
-
-
-  React.useEffect(() => {
-    fetch('https://630a45113249910032837b93.mockapi.io/items')
-      .then((res) => { return res.json(); })
-      .then((arr) => {
-        setItems(arr)
-      });
-  }, []);
-
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
-          </div>
-        </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
       </div>
     </div>
   )
