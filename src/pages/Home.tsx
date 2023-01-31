75f5251f5baf5eb074f581380478ad5c8eb2a712
Bogdan <fbogdan13@gmail.com>
2023-01-31 19:19:41 +0300

    css changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,34 @@ import React from 'react'
 
 
 const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState(
+    {
+      name: 'популярности',
+      sortProperty: 'rating'
+    }
+  );
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+
+    fetch(
+        `https://630a45113249910032837b93.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+    )
+        .then((res) => res.json())
+        .then((arr) => {
+            setItems(arr)
+            setIsLoading(false);
+        });
+    window.scrollTo(0, 0);
+}, [categoryId, sortType]);
+
   return (
     <div></div>
   )
