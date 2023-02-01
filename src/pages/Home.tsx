54f5f94a38aba894fc3168e38442337ac9723eba
Bogdan <fbogdan13@gmail.com>
2023-02-01 17:48:37 +0300

    database fix + minor changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import Categories from '../components/Categories.tsx'
+import Sort from '../components/Sort.tsx'
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
 
 const Home = () => {
   const [items, setItems] = React.useState([]);
@@ -20,18 +25,29 @@ const Home = () => {
     const category = categoryId > 0 ? `category=${categoryId}` : '';
 
     fetch(
-        `https://630a45113249910032837b93.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://630a45113249910032837b93.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
     )
-        .then((res) => res.json())
-        .then((arr) => {
-            setItems(arr)
-            setIsLoading(false);
-        });
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr)
+        setIsLoading(false)
+      });
     window.scrollTo(0, 0);
-}, [categoryId, sortType]);
+  }, [categoryId, sortType]);
 
   return (
-    <div></div>
+    <div className="container">
+      <div className="content__top">
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
   )
 }
 
